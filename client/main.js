f2e900e0e563b1d115112f69f48a6990a1798f16
David Obodo <obododavid5@gmail.com>
2020-04-27 09:17:42 +0100

    commit

---
+++ client/main.js
@@ -0,0 +1,35 @@
+const form = document.forms[0];
+const button = document.querySelector('button');
+
+
+const handleOnSubmit = (e) => {
+    e.preventDefault();
+
+    const data = {
+        fullName: form.name.value,
+        email: form.email.value,
+        subject: form.subject.value,
+        message: form.message.value
+    }
+
+
+    const headers = new Headers({
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+    })
+
+    const request = new Request(form.action, {
+        method: form.method,
+        mode: "no-cors",
+        headers: headers,
+        body: JSON.stringify(data),
+    })
+
+    fetch(request)
+        .then(res => console.log(res.json()))
+        .then(data => console.log(data))
+        .catch(err => console.log(err))
+
+}
+
+button.addEventListener('click', handleOnSubmit)
---
+++ index.js
@@ -0,0 +1,68 @@
+var PORT = process.env.PORT || 5000;
+const http = require('http')
+const express = require('express');
+const nodemailer = require('nodemailer');
+const StringDecoder = require('string_decoder').StringDecoder;
+require('dotenv').config();
+
+const app = express();
+const server = http.Server(app);
+
+
+app.use(express.static('client'));
+
+app.get('/test', (req, res) => {
+})
+
+app.post('/send', (req, res) => {
+
+    var decoder = new StringDecoder('utf-8');
+
+    var buffer = '';
+
+    req.on('data', function (data) {
+        buffer += decoder.write(data);
+    })
+
+    req.on('end', function () {
+        buffer += decoder.end();
+
+        var parsedBuffer = JSON.parse(buffer);
+
+        const { fullName, email, subject, message } = parsedBuffer
+        let output = `
+                <p>${message}</p>
+            `;
+
+        let transporter = nodemailer.createTransport({
+            service: "gmail",
+            port: 465,
+            secure: true,
+            auth: {
+                user: process.env.USER_NAME,
+                pass: process.env.PASSWORD
+            },
+            tls: {
+                rejectUnauthorized: false
+            }
+        });
+
+        let mailOptions = {
+            from: `${fullName} <${email}>`,
+            to: `${email}, davidobodo@rocketmail.com`,
+            subject: `${subject}`,
+            text: "What is this used for?",
+            html: output
+        };
+
+        transporter.sendMail(mailOptions, (err, data) => {
+            if (err) {
+                return console.log('Error occurs');
+            }
+            return console.log('Email sent!!!', data);
+        });
+    })
+})
+
+
+server.listen(PORT, () => console.log('Server started'))
